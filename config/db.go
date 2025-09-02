49a99f0fa29a428ba515ec49064c50f380c36d6d
abhishek-mehta-dev <mehtaabhishek499@gmail.com>
2025-09-02 17:00:56 +0530

    Make the database connection to connect the mysql server

---
+++ config/db.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func ConnectDB() {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	var err error
+	DB, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Failed to connect to DB: ", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("DB not reachable: ", err)
+	}
+
+	log.Println("Connected to MySQL")
+}
